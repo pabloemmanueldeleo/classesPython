4d1cdbc25e99f799fba8158cb1683abf3ba9f8cb
Pablo Emmanuel De Leo <pabloemmanueldeleo@gmail.com>
2018-04-07 13:10:51 -0300

    no lo habia guardado

--- modulosGenericos/namingConv.py
+++ modulosGenericos/namingConv.py
@@ -1,27 +1,18 @@
+# @Date:   2018-04-07T11:03:42-03:00
+# @Last modified time: 2018-04-07T13:10:31-03:00
+
+
+
 import maya.cmds as mc
 
-def namingConv ( side = '', part = '', desc = '', node = '', conv = ''):
-
+def namingConv ( side = 'L', part = 'BODY', desc = 'ARM'):
+    '''
+    side(String) : Side part
+    part(String) : part mesh
+    desc(String) : description object
+    node(String) : type node resume
+    '''
     name = 'side_part_desc__node'
-
-    if conv == 'A':
-        name = 'side_part_desc__node'
-    if conv == 'B':
-        name = 'node__part_desc_side'
-    if conv == 'C':
-        name = 'side_partdesc__node'
-    if conv == 'D':
-        name = 'side_partdesc__node'
-
-    if side:
-        name = name.replace ( 'side', side)
-    if part:
-        name = name.replace ( 'part', part)
-    if desc:
-        name = name.replace ( 'desc', desc)
-    if node:
-        name = name.replace ( 'node', node)
-
     obj = mc.ls ( sl = 1 )
     if len ( obj ) == 1:
         shape = mc.listRelatives ( obj, s = 1)
@@ -33,6 +24,11 @@ def namingConv ( side = '', part = '', desc = '', node = '', conv = ''):
         obj = mc.rename ( obj, name)
 
     return name
+
+def nameingSuf(node = 'MSH'):
+
+
+
 
 '''
 name = namingConv ( 'L',  'FFF', 'AAA', '','A')
